bfdf27fccbd6c082958e55a059472f18fb5e0bc7
renzo01 <33385098+renzo01@users.noreply.github.com>
2019-03-17 22:13:26 -0500

    Add files via upload

--- 07-MathTraning/07-MathTraning/Controller/ViewController.swift
+++ 07-MathTraning/07-MathTraning/Controller/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
-
+//created for able to work with machine learning, models...
+import Vision
 class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var drowingView: DrawingImageView!
+    //for using the data of IA
+    var nistModel = MnistModel()
     //references factory
     let factory = QuestionsFactory()
     override func viewDidLoad() {
@@ -26,7 +29,52 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     }
 
     func numberDrown(_ image: UIImage){
-        askNextQuestion()
+        //1. resize the user image to an image 299 x 299, size what request the model
+        let modelSize = 299
+        //scaler....
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: modelSize, height: modelSize), true, 1.0)
+        //indicate UIViewController works in this space.
+        image.draw(in: CGRect(x: 0, y: 0, width: modelSize, height: modelSize))
+        //only use "!" if you are sure existe the option.
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        //close the context
+        UIGraphicsEndImageContext()
+        //2. UIImage -> CImage
+        guard let ciImage = CIImage(image: newImage) else {
+            fatalError("Error when convert UIImage to CImage")
+        }
+        //3. load the model in vision.
+        guard let model = try? VNCoreMLModel(for: nistModel.model) else{
+            fatalError("No se a podido preparar el modelo")
+        }
+        //4. make a request of VNCoreMLRequest
+        let request = VNCoreMLRequest(model: model) { [weak self] (request, error) in
+             //5. when ditected the image, we have to validate the answere
+            guard let result = request.results as?[VNClassificationObservation],
+                let prediction = result.first else{
+                    fatalError("Error al hacer la prediccion. \(error?.localizedDescription ?? "error desconocido")")
+            }
+            DispatchQueue.main.async {
+                //El resultado es la etiqueta de la classe donde el modelo ha catalogado nuestra imagen
+                let result = Int(prediction.identifier) ?? 0
+                //asssing user answer for actual question
+                self?.factory.updateQuestion(at: 0, with: result)
+                //validate if the userAnswer is correct.
+                self?.factory.validateQuestion(at: 0)
+                //create a new Question.
+                self?.askNextQuestion()
+            }
+        }
+        //6. connect everything in an object type VNRequestHandler
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        //execute the request in a second thread for dont stop the UI
+        DispatchQueue.global(qos: .userInteractive).async {
+            do{
+                try handler.perform([request])
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
     }
     
     func askNextQuestion(){
--- 07-MathTraning/07-MathTraning/Model/QuestionsFactory.swift
+++ 07-MathTraning/07-MathTraning/Model/QuestionsFactory.swift
@@ -15,6 +15,7 @@ class QuestionsFactory{
     //(set) =  setter
     private(set) var score: Int = 0
     
+    
     init() {
         self.addNewQuestion()
     }
@@ -59,5 +60,13 @@ class QuestionsFactory{
             
         }
         return Question(text: question, Answer : correctAnswer, userAnswer: nil)
+    }
+    func updateQuestion (at index: Int, with answer: Int){
+        questions[index].userAnswer = answer
+    }
+    func validateQuestion (at index: Int){
+        if self.questions[index].userAnswer == self.questions[index].Answer{
+            self.score += 100 
+        }
     }
 }
